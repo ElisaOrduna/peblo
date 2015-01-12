b708c1a161329e8bcb5e7df08540ceb71ff608ae
ElisaOrduna <chaubuenosaires@hotmail.com>
2015-01-12 18:38:35 -0300

    tokenizador

--- parser.cpp
+++ parser.cpp
@@ -11,6 +11,12 @@ typedef enum {
 	/* Palabras reservadas */
 	TOK_FUN,
 	TOK_LET,
+	TOK_IN,
+	TOK_VAR,
+	TOK_TYPE,
+	TOK_CASE,
+	TOK_OF,
+	TOK_END,
 	/* Operadores */
 	TOK_DEF,	// =
 	TOK_EQ,		// ==
@@ -21,6 +27,19 @@ typedef enum {
 	TOK_GT,		// >
 	TOK_NOT,	// !
 	TOK_FAT_ARROW,	// =>
+	TOK_LPAREN,	// (
+	TOK_RPAREN,	// )
+	TOK_COLON,	// :
+	TOK_SEMICOLON,	// ;
+	TOK_PLUS,	// +
+	TOK_MINUS,	// -
+	TOK_MUL,	// *
+	TOK_DIV,	// /
+	TOK_MOD,	// %
+	TOK_AND,	// &&
+	TOK_OR,		// ||
+	TOK_PIPE,	// |
+	TOK_ARROW,	// ->
 } TokenType;
 
 struct Token {
@@ -92,6 +111,18 @@ void Tokenizer::next(void) {
 			_last_token.type = TOK_FUN;
 		} else if (s == "let") {
 			_last_token.type = TOK_LET;
+		} else if (s == "in") {
+			_last_token.type = TOK_IN;
+		} else if (s == "case") {
+			_last_token.type = TOK_CASE;
+		} else if (s == "of") {
+			_last_token.type = TOK_OF;
+		} else if (s == "end") {
+			_last_token.type = TOK_END;
+		} else if (s == "var") {
+			_last_token.type = TOK_VAR;
+		} else if (s == "type") {
+			_last_token.type = TOK_TYPE;
 		} else {
 			// No es una palabra reservada
 			if (IS_UPPER(s[0])) {
@@ -105,32 +136,107 @@ void Tokenizer::next(void) {
 		if (_char == '=') {
 			_char = _is.get();
 			_last_token.type = TOK_LE;
+			_last_token.value = "<=";
 		} else {
 			_last_token.type = TOK_LT;
+			_last_token.value = "<";
 		}
 	} else if (_char == '>'){
 		_char = _is.get();
 		if (_char == '=') {
 			_char = _is.get();
 			_last_token.type = TOK_GE;
+			_last_token.value = ">=";
 		} else {
 			_last_token.type = TOK_GT;
+			_last_token.value = ">";
 		}
 	} else if (_char == '!') {
 		_char = _is.get();
 		if (_char == '=') {
+			_char = _is.get();
 			_last_token.type = TOK_NE;
+			_last_token.value = "!=";
 		} else {
 			_last_token.type = TOK_NOT;
+			_last_token.value = "!";
 		}
 	} else if (_char == '=') {
 		_char = _is.get();
 		if (_char == '>') {
+			_char = _is.get();
 			_last_token.type = TOK_FAT_ARROW;
+			_last_token.value = "=>";
 		} else if (_char == '=') {
+			_char = _is.get();
 			_last_token.type = TOK_EQ;		
+			_last_token.value = "==";
 		} else {
 			_last_token.type = TOK_DEF;		
+			_last_token.value = "=";
+		}
+	} else if (_char == '(') {
+		_char = _is.get();
+		_last_token.type = TOK_LPAREN;
+		_last_token.value = "(";
+	} else if (_char == ')') {
+		_char = _is.get();
+		_last_token.type = TOK_RPAREN;
+		_last_token.value = ")";
+	} else if (_char == ':') {
+		_char = _is.get();
+		_last_token.type = TOK_COLON;
+		_last_token.value = ":";
+	} else if (_char == ';') {
+		_char = _is.get();
+		_last_token.type = TOK_SEMICOLON;
+		_last_token.value = ";";
+	} else if (_char == '+') {
+		_char = _is.get();
+		_last_token.type = TOK_PLUS;
+		_last_token.value = "+";
+	} else if (_char == '-') {
+		_char = _is.get();
+		if (_char == '>') {
+			_char = _is.get();
+			_last_token.type = TOK_ARROW;
+			_last_token.value = "->";
+		} else {
+			_last_token.type = TOK_MINUS;
+			_last_token.value = "-";
+		}
+	} else if (_char == '*') {
+		_char = _is.get();
+		_last_token.type = TOK_MUL;
+		_last_token.value = "*";
+	} else if (_char == '/') {
+		_char = _is.get();
+		_last_token.type = TOK_DIV;
+		_last_token.value = "/";
+	} else if (_char == '%') {
+		_char = _is.get();
+		_last_token.type = TOK_MOD;
+		_last_token.value = "%";
+	} else if (_char == '&') {
+		_char = _is.get();
+		if (_char == '&') {
+			_char = _is.get();
+			_last_token.type = TOK_AND;
+			_last_token.value = "&&";
+		} else {
+			cerr << "Error: Simbolo no reconocido en la entrada: "
+				<< "'" << (char)_char << "' (" << (int)_char << ")" << endl;
+			exit(1);
+		}
+	} else if (_char == '|') {
+		_char = _is.get();
+		if (_char == '|') {
+			_char = _is.get();
+			_last_token.type = TOK_OR;
+			_last_token.value = "||";
+		} else {
+			_last_token.type = TOK_PIPE;
+			_last_token.value = "|";
 		}
 	} else {
 		cerr << "Error: Simbolo no reconocido en la entrada: "
@@ -140,12 +246,13 @@ void Tokenizer::next(void) {
 }
 
 #include <fstream>
+
 int main() {
 	ifstream input("test.txt");
 	Tokenizer tok(input);
 	while (!tok.eof()) {
 		Token t = tok.peek();
-		cout << "lei <type=" << t.type << ", value=" << t.value << ">" << endl;
+		cout << "lei <type=" << t.type << ", value=[[ " << t.value << " ]]>" << endl;
 		tok.next();
 	}
 	return 0;
