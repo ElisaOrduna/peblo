4465de6e79faa1a3702c03bd9450e26cb42d8519
ElisaOrduna <chaubuenosaires@hotmail.com>
2015-01-13 22:42:26 -0300

    parser: funciones y tipos

--- parser.cpp
+++ parser.cpp
@@ -11,7 +11,15 @@ typedef enum {
 	AST_APP,
 	AST_CASE,
 	AST_FUN,
+	AST_PARAMETER,
+	AST_TYPE_CONSTRUCTOR,
+	AST_TYPE_VAR,
+	AST_TYPE_ARROW,
 } ASTKind;
+
+#define AST_FUN_PARAM		0
+#define AST_FUN_RETTYPE		1
+#define AST_FUN_BODY		2
 
 struct AST {
 	ASTKind kind;
@@ -27,14 +35,19 @@ void AST::show(ostream& os, unsigned int level) const {
 		os << "  ";
 	}
 	os << "(" << kind << " " << token.type << " " << token.value;
-	
 	for (i = 0; i < children.size(); i++) {
 		os << endl;
-		children[i]->show(os, level + 1);
+		if (children[i] != NULL) { 
+			children[i]->show(os, level + 1);
+		} else {
+			unsigned int j;
+			for (j = 0; j < level + 1; j++) {
+				os << "  ";
+			}
+			os << "NULL";
+		}
 	}
-
 	os << ")"; 
-
 }
 
 class Parser {
@@ -43,12 +56,16 @@ class Parser {
 		
 		AST* parse_atom(void);
 		AST* parse_expression(void);
+		AST* parse_type(void);
+		AST* parse_type_atom(bool allow_parameters);
+		AST* parse_fun(void);
+		AST* parse_parameter(void);
 	private:
 		Tokenizer& _tokenizer;
+		void assert_type(TokenType t);
 };
 
-Parser::Parser(Tokenizer& t) : _tokenizer(t) {
-}
+Parser::Parser(Tokenizer& t) : _tokenizer(t) {}
 
 AST* Parser::parse_atom(void) {
 	Token t = _tokenizer.peek();
@@ -93,6 +110,128 @@ bool is_terminator(TokenType t) {
 		||	t == TOK_RPAREN;
 }
 
+bool is_parameter_terminator(TokenType t) {
+	return		t == TOK_FAT_ARROW
+		||	t == TOK_COLON;
+}
+
+bool is_type_terminator(TokenType t) {
+	return		t == TOK_RPAREN
+		||	t == TOK_ARROW
+		||	t == TOK_FAT_ARROW
+		||	t == TOK_DEF;
+}
+
+void Parser::assert_type(TokenType t) {
+	if (_tokenizer.peek().type != t) {
+		cerr << "Error de sintaxis" << endl;
+		cerr << "Se esperaba token " << t << endl;
+		cerr << "Se recibio token " << _tokenizer.peek().type << endl;
+		exit(1);
+	}
+}
+
+AST* Parser::parse_type(void) {
+	AST* res = parse_type_atom(true);
+	if (_tokenizer.peek().type == TOK_ARROW) {
+		_tokenizer.next();
+		AST* aux = new AST();
+		aux->kind = AST_TYPE_ARROW;
+		aux->children.push_back(res);
+		aux->children.push_back(parse_type());
+		res = aux;
+	}
+	return res;
+}
+
+AST* Parser::parse_type_atom(bool allow_parameters) {
+	if (_tokenizer.peek().type == TOK_LPAREN) {
+		_tokenizer.next();
+		AST* res = parse_type();
+		assert_type(TOK_RPAREN);
+		_tokenizer.next();
+		return res;
+	} else if (_tokenizer.peek().type == TOK_UPPERID) {
+		AST* res = new AST();
+		res->kind = AST_TYPE_CONSTRUCTOR;
+		res->token = _tokenizer.peek();
+		_tokenizer.next();
+		if (allow_parameters) {
+			while (!is_type_terminator(_tokenizer.peek().type)) {
+				res->children.push_back(parse_type_atom(false));
+			}
+		}
+		return res;
+	} else {
+		assert_type(TOK_LOWERID);
+		AST* res = new AST();
+		res->kind = AST_TYPE_VAR;
+		res->token = _tokenizer.peek();
+
+		_tokenizer.next();
+		return res;
+	}
+}
+
+AST* Parser::parse_parameter(void) {
+	AST* res = new AST();
+	res->kind = AST_PARAMETER;
+	if (_tokenizer.peek().type == TOK_LPAREN) {
+		_tokenizer.next();
+
+		assert_type(TOK_LOWERID);
+		res->token = _tokenizer.peek(); // Nombre parametro
+		_tokenizer.next();
+		
+		assert_type(TOK_COLON);
+		_tokenizer.next();
+
+		res->children.push_back(parse_type()); // Tipo parametro
+
+		assert_type(TOK_RPAREN);
+		_tokenizer.next();
+	} else {
+		assert_type(TOK_LOWERID);
+		res->token = _tokenizer.peek(); // Nombre parametro
+		_tokenizer.next();
+
+		res->children.push_back(NULL); // Tipo parametro
+	}
+	return res;
+}
+
+AST* Parser::parse_fun(void) {
+	_tokenizer.next();
+
+	AST* res = new AST();
+	res->kind = AST_FUN;
+	res->children.push_back(parse_parameter());
+	res->children.push_back(NULL); // Tipo de retorno
+	res->children.push_back(NULL); // Cuerpo de la funcion
+	AST* last = res;
+	while (!is_parameter_terminator(_tokenizer.peek().type)) {
+		AST* aux = new AST();
+		aux->kind = AST_FUN;
+		aux->children.push_back(parse_parameter());
+		aux->children.push_back(NULL); // Tipo de retorno
+		aux->children.push_back(NULL); // Cuerpo de la funcion
+		
+		last->children[AST_FUN_BODY] = aux;
+		last = aux;
+	}
+	
+	if (_tokenizer.peek().type == TOK_COLON) {
+		_tokenizer.next();
+		last->children[AST_FUN_RETTYPE] = parse_type();
+	}
+	
+	assert_type(TOK_FAT_ARROW);
+	_tokenizer.next();
+
+	last->children[AST_FUN_BODY] = parse_expression();
+	return res;
+}
+
 AST* Parser::parse_expression(void) {
 	Token t = _tokenizer.peek();
 
@@ -101,21 +240,16 @@ AST* Parser::parse_expression(void) {
 		cout << "es un case" << endl;
 		//TODO
 		return res;
+	} else if (t.type == TOK_FUN) {
+		return parse_fun();
 	} else {
 		// Aplicacion
-		vector<AST*> exprs;
-		exprs.push_back(parse_atom());
+		AST* res = parse_atom();
 		while (!is_terminator(_tokenizer.peek().type)) {
-			exprs.push_back(parse_atom());
-		}
-	
-		AST* res = exprs[0];
-		unsigned int i;
-		for (i = 1; i < exprs.size(); i++) {
 			AST* ast = new AST();
 			ast->kind = AST_APP;
 			ast->children.push_back(res);
-			ast->children.push_back(exprs[i]);
+			ast->children.push_back(parse_atom());
 			res = ast;
 		}
 		return res;
